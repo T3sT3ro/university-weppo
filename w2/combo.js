0492ed8fb91732626af91b4a919d6e85cd8900d1
Tooster <max@polarczyk.pl>
2020-01-13 03:18:03 +0100

    add w2

---
+++ w2/combo.js
@@ -0,0 +1,17 @@
+
+let x = 1
+
+if(x == 1){
+    let x = 2
+    let a = [1, 2, 3]
+    a[2]
+    // a.2
+}
+
+let a = [1, 2, 3]
+console.log(a[5])
+var foo = {prop: 2}
+a[foo] = 17
+console.log(a[foo])
+a.length = 2
+console.log([![]]+[][[]])
---
+++ w2/object-keys.js
@@ -0,0 +1,7 @@
+const car = {
+    make: 'Honda',
+    model: 'Camry',
+    year: 2020
+}
+
+console.log(Object.keys(car))
